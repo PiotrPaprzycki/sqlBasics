ad0cd08e02844b1455c2d95d1b30d1e4617719dd
PiotrPaprzycki <Avalok2015>
2021-04-14 16:17:56 +0200

    Second

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@ CREATE TABLE pracownik (
     imie VARCHAR(30),
     nazwisko VARCHAR(50),
     wyplata DECIMAL,
-    data_urodzenia VARCHAR(10),
+    data_urodzenia DATE,
     stanowisko VARCHAR(50)
 );
 
@@ -23,6 +23,58 @@ SELECT * FROM pracownik WHERE data_urodzenia <= 1991-01-01;
 
 UPDATE pracownik SET wyplata= wyplata + (wyplata * 10 / 100);
 
-DELETE FROM pracownik
-WHERE pracownik.id =  (SELECT id FROM (SELECT id,min(data_urodzenia) FROM pracownik) AS id);
+DELETE FROM pracownik WHERE  pracownik.id =  (SELECT id FROM (SELECT id, data_urodzenia
+FROM pracownik
+WHERE data_urodzenia = (
+    SELECT MIN(data_urodzenia)
+    FROM pracownik
+)) AS id);
+
+
+
+DROP TABLE pracownik;
+DROP TABLE stanowisko;
+DROP TABLE adres;
+
+CREATE TABLE stanowisko (
+	id bigint  primary key auto_increment UNIQUE NOT NULL,
+    nazwa VARCHAR(30),
+    wyplata DECIMAL
+);
+
+CREATE TABLE adres (
+	id bigint primary key auto_increment UNIQUE NOT NULL,
+    ulica VARCHAR(70),
+    numer_domu VARCHAR(5),
+    numer_mieszkania VARCHAR(5),
+    miejscowosc VARCHAR(100),
+    kod_pocztowy VARCHAR(5)
+);
+
+CREATE TABLE pracownik (
+	id BIGINT  primary key auto_increment UNIQUE NOT NULL,
+	adres_id BIGINT UNIQUE NOT NULL,
+	stanowisko_id BIGINT UNIQUE NOT NULL,
+    imie VARCHAR(30),
+    nazwisko VARCHAR(30),
+    FOREIGN KEY (adres_id) REFERENCES adres(id),
+    FOREIGN KEY (stanowisko_id) REFERENCES stanowisko(id)
+);
+
+INSERT INTO adres(ulica, numer_domu, numer_mieszkania, miejscowosc, kod_pocztowy)
+VALUES ('Jana Matejki',1,2,'Wroclaw',54-062),
+('Jana Paska',2,1,'Wloclawek',11-111),
+('Jana Sedzimira',2,3,'Warszawa',12-222);
+
+INSERT INTO stanowisko(nazwa, wyplata)
+VALUES ('Junior DEV',5000),
+('Regular DEV',8000),
+('Senior DEV',12000);
+
+INSERT INTO pracownik(adres_id, stanowisko_id, imie, nazwisko)
+VALUES (1,1,'Piotr','Skarga'),
+(2,2,'Jan','Kowalski'),
+(3,3,'Kamil','Gruszka');
+
+SELECT * FROM pracownik,adres,stanowisko WHERE pracownik.stanowisko_id=stanowisko.id AND pracownik.adres_id=adres.id;
 
